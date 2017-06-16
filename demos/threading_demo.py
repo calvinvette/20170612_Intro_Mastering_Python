0600a47288ee4c13d6b0dc5cefaf51acd57f47f9
calvinvette <calvinvette@gmail.com>
2017-06-16 23:03:48 +0000

    Fixed Threading Demo

--- demos/threading_demo.py
+++ demos/threading_demo.py
@@ -7,32 +7,36 @@ done = [
 ]
 
 
-def my_thread(*url_to_download):
+def my_thread(url_to_download):
+    # print('xxx: ', url_to_download)
     time.sleep(random.randint(1, 10))
+    # print('yyy: ', url_to_download)
     done.append(url_to_download)
 
-urls = [
+urls = (
     r'http://google.com',
     r'http://google.com/logo.png',
     r'http://google.com/main.js',
     r'http://google.com/site.css'
-]
+)
 
 for url in urls:
-    t = threading.Thread(target=my_thread, args=(url))
+    t = threading.Thread(target=my_thread, args=[url])
     t.daemon = True
+    print('Starting download of:', url)
     t.start()
 
 done_len = len(done)
 url_len = len(urls)
-print(url_len)
-while done_len <= url_len:
+# print("%d of %d" % (done_len, url_len))
+
+while done_len < url_len:
     # print('-' * 40)
-    print("%d: %s" % (len(done), done), end='\r')
+    print("%d: %s" % (done_len, done))
     time.sleep(1)
     done_len = len(done)
 
-
+print("%d: %s" % (done_len, done))
 print("Done!")
 
 
